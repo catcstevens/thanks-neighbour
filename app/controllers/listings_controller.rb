e4d011a315c68d6a00c560110f22899adf2b40a0
Cat <ccallcottstevens@gmail.com>
2020-02-28 13:52:05 +1100

    added listings controller and views

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,65 @@
+class ListingsController < ApplicationController
+    
+    before_action :set_listing, only: [:show, :edit, :update, :destroy]
+    before_action :set_conditions, only: [:new, :edit]
+        def index
+            @listings = Listing.all
+        end
+    
+        def show
+            
+        end
+    
+        def new
+            @listing = Listing.new
+        end
+    
+        def create
+            @listing = Listing.create(listing_params)
+                console
+            if @listing.errors.any?
+                render "new"
+            else
+                redirect_to listings_path
+            end
+    
+        end
+    
+        def edit
+            
+        end
+    
+        def update
+            
+            
+            if @listing.update(listing_params)
+                redirect_to @listing
+            else
+                render 'edit'
+            end
+        end
+    
+        def destroy
+            
+            
+            @listing.destroy
+     
+            redirect_to listings_path
+        end
+    
+        private
+    
+        def set_listing
+            id = params[:id]
+            @listing = Listing.find(id)
+        end
+        def set_conditions
+            @condition = Listing.conditions.keys
+        end
+    
+        def listing_params
+            params.require(:listing).permit(:title, :description, :condition, :price, :deposit, :from, :to, :delivery, :min_rental, :neighbour_id, :photo_id)
+        end
+    
+    end
+  
